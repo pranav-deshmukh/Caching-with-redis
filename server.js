7ce8306949ef9111efa7f8f2c0ceb7915f0d2e41
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-06-28 19:16:22 +0530

    feat: caching using redis

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const client = require("./client");
 
 app.get("/", async (req, res) => {
+  const cacheValue = await client.get("todos");
+  if (cacheValue) {
+    return res.json(JSON.parse(cacheValue));
+  }
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/todos/"
   );
+  await client.set("todos", JSON.stringify(data));
+  await client.expire("todos", 30);
   return res.json(data);
 });
 
